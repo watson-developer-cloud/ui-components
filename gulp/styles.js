d173798f33bc14a9afd26321a6dac406c34a7b49
JAMES ZHANG <jzhang300@us.ibm.com>
2016-07-25 15:34:30 -0500

    fix sass compiling gulp task

--- gulp/styles.js
+++ gulp/styles.js
@@ -7,6 +7,7 @@
 var config = require('../config.js'),
     sass = require('./_styles.js'),
     gulp = require('gulp'),
+    clean = require('gulp-clean'),
     browserSync = require('browser-sync'),
     runSequence = require('run-sequence'),
     onError = require('./on-error.js');
@@ -16,7 +17,16 @@ var watchPath = config.paths.src.styles + '/**/*.scss',
     destPath = config.paths.build.styles;
 
 gulp.task('styles', function() {
+  return runSequence('styles:compile', 'styles:clean');
+});
+
+gulp.task('styles:compile', function() {
   return sass(watchPath, destPath, gulp);
+});
+
+gulp.task('styles:clean', function() {
+  return gulp.src(destPath + '/watson-components.css')
+    .pipe(clean());
 });
 
 gulp.task('styles:watch', function() {
