dd4a32612d3c4c81907c13a0c48874ef646779bf
James Zhang <james.zhang75024@gmail.com>
2016-08-05 15:18:51 -0500

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,4 @@ git config --global user.email "james.zhang75024@gmail.com"
 git config --global user.name "James Zhang"
 
 # deploy
-git subtree push "https://${GITHUB_TOKEN}@$github.com/ui-components.git" --prefix docs origin gh-pages
+git subtree push "https://${GH_TOKEN}@$github.com/ui-components.git" --prefix docs origin gh-pages
