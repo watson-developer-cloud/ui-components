7d04bffbf3bc37db670c5fc1ca548a518e147a37
James Zhang <james.zhang75024@gmail.com>
2016-08-09 11:07:18 -0500

    update travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-GH_REPO="@github.com/jzhang300/ui-components.git"
+GH_REPO="@github.com/watson-developer-cloud/ui-components.git"
 FULL_REPO="https://$GH_TOKEN$GH_REPO"
 
 gulp compile
 cd docs
 git init
 git config user.name "Travis Ci"
-git config user.email "james.zhang75024@gmail.com"
+git config user.email "wps@us.ibm.com"
 
 git add .
 git commit -m "deployed to github pages"
